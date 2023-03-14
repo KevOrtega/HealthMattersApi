00b131b604b592e51dd3fcc6172830bf0615c3bc
manuozan <manuelaozaan@hotmail.com>
2023-03-14 20:24:23 -0300

    patient & routes fixed

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -3,13 +3,7 @@ import { getDoctor, postDoctors } from "../controllers/item";
 
 const router = Router();
 
-<<<<<<< HEAD
-// router.get('/doctors', getDoctors)
-router.get('/doctors/:id', getDoctor)
-router.post('/doctors', postDoctors)
-=======
-router.get("/", getDoctors);
+router.get("/", getDoctor);
 router.post("/", postDoctors);
->>>>>>> lilieth
 
 export default router;
