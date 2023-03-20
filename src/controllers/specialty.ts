753effdd4fd7c10855f9e1ad83c329a3e5d35400
lilieth10 <liliethramirez91@gmail.com>
2023-03-20 15:34:21 -0300

    prueba de filtros y ordenamiento en specialidades.

--- src/controllers/specialty.ts
+++ src/controllers/specialty.ts
@@ -1,27 +1,90 @@
 import { Request, Response } from "express";
+import specialities from "../data";
 import DoctorModel from "../models/doctor";
-import SpecialtyModel from "../models/specialty";
+import SpecialtyModel, { Specialty } from "../models/specialty";
+
+type getSpecialtyQueries = {
+	doctors?: string;
+	search?: string;
+	order?: "nameASC" | "nameDESC";
+	page?: string;
+};
 
 const getSpecialty = async (req: Request, res: Response) => {
 	try {
-		const specialities = await SpecialtyModel.find().populate("name");
-		res.json(specialities);
+		const { doctors, search, order, page }: getSpecialtyQueries = req.query;
+
+		const specialtiesPerPage = 6;
+		const pageNumber = parseInt(page as string, 10) || 1;
+
+		const orders_methods = {
+			nameASC: (arr: Specialty[]) => arr.sort((a, b) => a.name.localeCompare(b.name)),
+			nameDESC: (arr: Specialty[]) => arr.sort((a, b) => b.name.localeCompare(a.name)),
+		};
+
+		const doctorsArray: string[] | undefined = doctors ? (Array.isArray(doctors) ? doctors : [doctors]) : undefined;
+
+		const search_params = Object.assign(
+			{},
+			search
+				? {
+						name: new RegExp(`^${search}$`, "i"),
+				  }
+				: {},
+			doctorsArray ? { doctors: { $in: doctorsArray } } : {}
+		);
+
+		const specialties = order
+			? orders_methods[order](await SpecialtyModel.find(search_params))
+			: await SpecialtyModel.find(search_params);
+
+		const specialtiesCount = specialties.length;
+		const specialtiesToSkip = specialtiesPerPage * (pageNumber - 1);
+
+		res.status(200).send({
+			specialties: specialties.slice(specialtiesToSkip, specialtiesToSkip + specialtiesPerPage),
+			currentPage: pageNumber,
+			pages: Math.ceil(specialtiesCount / specialtiesPerPage),
+			count: specialtiesCount,
+		});
 	} catch (error) {
 		res.status(404).send({ message: error });
 	}
 };
 
+// const getSpecialty = async (req: Request, res: Response) => {
+// 	try {
+// 		const specialities = await SpecialtyModel.find().populate("name");
+// 		res.json(specialities);
+// 	} catch (error) {
+// 		res.status(404).send({ message: error });
+// 	}
+// };
+
 const postSpecialty = async (req: Request, res: Response) => {
 	try {
-		//  const specialities = await data
 		const { name } = req.body;
-		const postSpecialities = new SpecialtyModel({ name });
-		const saveSpecialities = await postSpecialities.save();
-		res.status(200).send(saveSpecialities);
+		const postSpecialties = new SpecialtyModel({ name });
+		const saveSpecialties = await postSpecialties.save();
+		// Agregar especialidad a un Doctor
+		await DoctorModel.updateOne({ _id: req.body.doctorId }, { $push: { specialties: postSpecialties._id } });
+		res.status(200).send(saveSpecialties);
 	} catch (error) {
 		res.status(404).send({ message: error });
 	}
 };
+
+// const postSpecialty = async (req: Request, res: Response) => {
+// 	try {
+// 		//  const specialities = await data
+// 		const { name } = req.body;
+// 		const postSpecialities = new SpecialtyModel({ name });
+// 		const saveSpecialities = await postSpecialities.save();
+// 		res.status(200).send(saveSpecialities);
+// 	} catch (error) {
+// 		res.status(404).send({ message: error });
+// 	}
+// };
 
 const detailSpecialty = async (req: Request, res: Response) => {
 	try {
@@ -55,4 +118,3 @@ const assignSpecialty = async (req: Request, res: Response) => {
 };
 
 export { getSpecialty, postSpecialty, assignSpecialty, detailSpecialty, deleteSpecialty };
-
--- src/models/specialty.ts
+++ src/models/specialty.ts
@@ -3,19 +3,25 @@ import { Schema, model, Types, Model } from "mongoose";
 export interface Specialty {
 	name: string;
 	doctors: Types.ObjectId[] | string[];
+	rating: number;
 }
 
-const SpecialtySchema = new Schema<Specialty>(
-	{
-        name: {
-            type: String,
-            required: true,
-        },
-        doctors: [{
-            type: Schema.Types.ObjectId, ref: 'doctors'
-        }],
-    },
-);
+const SpecialtySchema = new Schema<Specialty>({
+	name: {
+		type: String,
+		required: true,
+	},
+	doctors: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "doctors",
+		},
+	],
+	rating: {
+		type: Number,
+		required: true,
+	},
+});
 
-const SpecialtyModel = model('specialties', SpecialtySchema)
-export default SpecialtyModel
+const SpecialtyModel: Model<Specialty> = model("specialties", SpecialtySchema);
+export default SpecialtyModel;
