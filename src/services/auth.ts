2d38763b2dbb108fef4822b4642c7f3af066db57
manuozan <manuelaozaan@hotmail.com>
2023-03-30 13:47:35 -0300

    fixed register users

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,11 +4,25 @@ import UserModel from "../models/auth";
 import { encrypt, verified } from "../utils/bcrypt";
 import { generateToken } from "../utils/jw.handle";
 import jwt from "jsonwebtoken"
-import PatientModel from "../models/patient";
-import DoctorModel from "../models/doctor";
 const JWT_SECRET = process.env.JWT_SECRET || "token.01010101";
 
-const registerNewUser = async ({ email, password, name, medicalLicense }: User) => {}
+
+const registerNewUser = async ({ email, password, name, medicalLicense }: User) => {
+  const checkIs = await UserModel.findOne({ email });
+  if (checkIs) {
+    throw new Error("Email already exists");
+  }
+  const passHash = await encrypt(password);
+  const newUser = await UserModel.create({
+    email,
+    password: passHash,
+    name,
+    medicalLicense
+  });
+  const token = jwt.sign({ email: newUser.email }, JWT_SECRET, { expiresIn: "60d" }); 
+  return { user: newUser, token };
+};
+
 
 
 
