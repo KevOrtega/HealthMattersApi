48acb8019c10226a72bc78e1ffe0277b746486b7
manuozan <manuelaozaan@hotmail.com>
2023-04-03 14:33:03 -0300

    Header ->  Cookies

--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -1,11 +1,20 @@
+import { verifyToken } from "../utils/jw.handle";
 import { NextFunction, Request, Response } from "express";
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-	const header = req.headers;
-	const userAgent = header["user-agent"];
-	console.log("user-agent", userAgent);
-
+	const jwtByUser = req.cookies.authorization || "";
+	const jwt = jwtByUser.split(" ").pop();
+	const isUser = verifyToken(`${jwt}`);
+  
+	if (isUser) {
+	  req.user = isUser;
+	  console.log("User authenticated");
+	} else {
+	  console.log("User not authenticated");
+	}
+  
 	next();
-};
+  };
+  
 
 export { logMiddleware };
--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getPatient, postPatient, getPatientList, assignPatient, deletePatient } from "../controllers/patient";
+import { getPatient, postPatient, getPatientList, deletePatient } from "../controllers/patient";
 
 const router = Router();
 
 router.get("/", getPatientList);
-router.put("/assignPatient/:_id", assignPatient);
+// router.put("/assignPatient/:_id", assignPatient);
 router.get("/:id", getPatient);
 router.post("/", postPatient);
 router.delete("/:_id", deletePatient);
